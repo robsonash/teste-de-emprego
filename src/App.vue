141d6b2f8689dd31d63fdc1a6599a01661ddb0b9
robsonash <robsoncs211@gmail.com>
2020-10-26 22:31:13 -0300

    parte de adicionar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,268 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="form">
+      <form @submit.prevent>
+        <label for="nome"><strong>Nome</strong></label>
+        <input
+          v-model="nome"
+          type="text"
+          id="nome"
+          name="nome"
+          placeholder="Digite seu nome..."
+        />
+
+        <label for="sobrenome"><strong>Sobrenome</strong></label>
+        <input
+          v-model="sobrenome"
+          type="text"
+          id="sobrenome"
+          name="sobrenome"
+          placeholder="Digite seu sobrenome..."
+        />
+
+        <label for="email"><strong>Email</strong></label>
+        <input
+          v-model="email"
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Digite seu email..."
+        />
+
+        <label for="telefone"><strong>Telefone</strong></label>
+        <input
+          v-model="telefone"
+          type="text"
+          id="telefone"
+          name="telefone"
+          placeholder="Digite seu telefone..."
+        />
+
+        <div class="checkbox">
+          <label class="checkbox">
+            <input v-model="checado" id="cnpj" type="checkbox" name="cnpj" />
+            CNPJ
+          </label>
+        </div>
+
+        <div v-if="checado">
+          <label :for="checado"></label>
+          <input
+            v-model="cnpj"
+            type="text"
+            id="cnpj"
+            name="cnpj"
+            placeholder="Coloque seu cnpj"
+          />
+        </div>
+
+        <div v-else>
+          <label for="cpf">cpf</label>
+          <input
+            v-model="cpf"
+            type="text"
+            id="checado"
+            name="checado"
+            placeholder="Coloque seu cpf"
+          />
+        </div>
+
+  
+          <input v-on:click="estado" type="submit" value="Submit" />
+     
+      
+       <!-- <div else class="buttons">
+       <button v-on:click="emitiralterar" class="button is-info"  type="button">Atualizar</button>
+
+</div> -->
+       
+
+
+
+
+
+      </form>
+    </div>
+    <div class="container-dados">
+      <div class="dado" v-for="cliente in clientes" :key="cliente.id">
+
+        <Cliente :cliente="cliente" @alterar="atualizarusuario($event)"  @delete="deletarusuario($event)" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cliente from "./components/Cliente.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      nome: "",
+      sobrenome: "",
+      email: "",
+      telefone: "",
+      razaoSocial: "",
+      checado: false,
+      cpf: "",
+      cnpj: "",
+      id: "",
+      clientes: [],
+      estadoform: "cadastrar",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Cliente,
+  },
+  //     watch:{
+  //    checadow:function(){
+
+  //      if(this.checado){
+
+  //       this.razaoSocial = this.cnpj;
+  //      // this.cpf = ''
+  //  console.log(this.razaoSocial);
+
+  //      }else{
+  //             this.razaoSocial = this.cpf;
+  //             //this.checado = false;
+  //             // this.cnpj = ''
+  //              console.log( this.razaoSocial);
+  //        }
+  //    }
+  //     },
+  methods: {
+    estado:function(){
+     if(this.estadoform == "cadastrar"){
+      this.adicionar()
+     }else{
+       this.atualizarusuario();
+       this.estadoform == "cadastrar";
+     }
+
+
+    },
+    adicionar: function () {
+      this.clientes.push({
+        nome: this.nome,
+        sobrenome: this.sobrenome,
+        email: this.email,
+        telefone: this.telefone,
+        id: Date.now(),
+      });
+        (this.nome = ""),
+        (this.sobrenome = ""),
+        (this.email = ""),
+        (this.telefone = ""),
+        console.log(this.clientes);
+    },
+    atualizarusuario: function($event){
+         var id = $event.idcliente;
+         console.log("id" + id );
+         this.nome =  $event.nome
+         this.sobrenome =  $event.sobrenome
+         this.email =  $event.email
+         this.telefone =  $event.telefone
+            console.log($event);
+      
+        console.log(this.clientes);
+    },
+
+    deletarusuario: function ($event) {
+      var id = $event.idcliente;
+      var listanova = this.clientes.filter((cliente) => cliente.id != id);
+      this.clientes = listanova;
+    },
+  },
+
+  computed: {},
+};
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+/* .form
+  {
+    border: 1px solid blue;
+    min-width: 30vw;
+    margin-right: 5px;
+  } */
+.formulario {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.container-dados {
+  width: auto;
+
+  /* min-width: 30vw; */
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px;
+}
+
+.dado {
+  margin-bottom: 20px;
+
+  width: 100%;
+}
+input[type="text"],
+select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type="email"],
+select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type="submit"] {
+  width: 100%;
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+
+.conteinerdados {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.dados {
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid green;
+}
+
+.container {
+  display: flex;
 }
 </style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,102 @@
+<template>
+
+
+<div>
+  <strong>Id: </strong> {{cliente.id}}
+  <strong>Nome: </strong> {{cliente.nome}}
+  <strong>Sobrenome:</strong>{{cliente.sobrenome}}
+  <strong>Email:</strong>{{cliente.email}}
+  <strong> Telefone:</strong>{{cliente.telefone}}
+  <div class="buttons">
+  <button v-on:click="emitiralterar" class="button is-warning"  type="button">Editar</button>
+ <button v-on:click="emitirdelete"  class="button is-danger" type="button">Excluir</button>
+</div>
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name:'Cliente',
+ data() {
+
+    return {
+ 
+    }
+ },
+ props:{
+   cliente: Object
+ },
+
+
+    methods:{
+      emitiralterar:function(){
+   this.$emit("alterar",{idcliente: this.cliente.id,nome: this.cliente.nome,sobrenome: this.cliente.sobrenome,email: this.cliente.email,telefone: this.cliente.telefone,component: this});
+      },
+     emitirdelete:function(){
+     this.$emit("delete",{idcliente: this.cliente.id,component: this});
+     }
+   
+
+
+
+    }, 
+
+    computed:{
+
+    }
+}
+    </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* .formulario{
+  width: 50%;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=email], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+.container{
+  display: flex;
+  
+} */
+
+
+
+
+
+
+</style>
